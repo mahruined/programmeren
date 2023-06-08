312c5d91c57225b2ae07698557201f1f9d0fc4b3
Marouane <99047391@mydavinci.nl>
2023-06-08 12:14:17 +0200

    assessment

--- dynamische-uis-main/ruit.js
+++ dynamische-uis-main/ruit.js
@@ -1,20 +1,22 @@
 function diamond(number) {
-    line='';
-    //up
-    for(i=1;i<=number;i++) {
-        for(j=1;j<=i;j++) {
-            line+=' '+j; //left   
-        }
-        line+='<br>';
+    let output = '';
+  
+    // Generate the top half of the diamond
+    for (let row = 1; row <= number; row++) {
+      for (let column = 1; column <= row; column++) {
+        output += ' ' + column; // Add numbers to the left of the diamond
+      }
+      output += '<br>'; // Start a new line
     }
-
-
-    //down
-    for(i=number-1;i>=1;i--) {
-        for(j=1;j<=i;j++) {
-        line+=' '+j; //left        
+  
+    // Generate the bottom half of the diamond
+    for (let row = number - 1; row >= 1; row--) {
+      for (let column = 1; column <= row; column++) {
+        output += ' ' + column; // Add numbers to the left of the diamond
+      }
+      output += '<br>'; // Start a new line
     }
-		line+='<br>';
-    }
-        document.getElementById('result').innerHTML=line;
-    } 
+  
+    // Display the diamond pattern
+    document.getElementById('result').innerHTML = output;
+  } 
