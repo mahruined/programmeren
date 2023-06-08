ebf52454eda1716f4ce5c9fbf57bfe70a12dca30
Marouane <99047391@mydavinci.nl>
2023-06-08 12:15:30 +0200

    assessment

---
+++ dynamische-uis-main/memory.js
@@ -0,0 +1,30 @@
+var cards = [
+    "assessment/death13.jpeg",
+    "assessment/hierophant.jpeg",
+    "assessment/silver_chariot.jpeg",
+    "assessment/star_platinum.jpeg",
+    "assessment/theworld.jpeg"
+  ];
+  
+  for (var i = 0; i < 5; i++) {
+    var img = document.createElement("img");
+    img.src = cards[i];
+    img.style.display = 'none'; // Hide the card image initially
+  
+    var button = document.createElement('button');
+    button.setAttribute('data-index', i); 
+    button.style.backgroundImage = 'url(assessment/backside.jpeg)';
+    button.style.width = '200px';
+    button.style.height = '200px';
+    button.style.backgroundSize = 'cover';
+  
+    button.addEventListener('click', function(e) {
+      var index = e.target.getAttribute('data-index');
+      var clickedImg = document.querySelector("img[src='" + cards[index] + "']"); 
+      clickedImg.style.display = 'block';
+      button.style.display = 'none';
+    });
+  
+    document.body.appendChild(button);
+    document.body.appendChild(img);
+  }
